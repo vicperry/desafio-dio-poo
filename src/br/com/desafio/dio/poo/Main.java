b9dce9db70e237b2715507d97ec485a3d5b1795b
Victor Pereira <vic.hugx@gmail.com>
2022-10-18 16:56:04 -0300

    Abstração e encapsulamento.

---
+++ src/br/com/desafio/dio/poo/Main.java
@@ -0,0 +1,30 @@
+package br.com.desafio.dio.poo;
+
+import java.time.LocalDate;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Curso curso1 = new Curso();
+		curso1.setTitulo("curso java");
+		curso1.setDescricao("descrição curso java");
+		curso1.setCargaHoraria(8);
+		
+		Curso curso2 = new Curso();
+		curso2.setTitulo("curso javascript");
+		curso2.setDescricao("descrição curso javascript");
+		curso2.setCargaHoraria(4);
+		
+		Mentoria mentoria = new Mentoria();
+		mentoria.setTitulo("mentoria de java");
+		mentoria.setDescricao("descrição mentoria java");
+		mentoria.setData(LocalDate.now());
+
+		System.out.println(curso1);
+		System.out.println(curso2);
+		System.out.println(mentoria);
+
+	}
+
+}
